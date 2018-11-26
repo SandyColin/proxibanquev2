a2d6dc38f6964f4db2d86e43b7d7020dd6e22bf6
SandyColin <sandycolin1990@gmail.com>
2018-11-26 16:08:01 +0100

    bootstrap

--- src/fr/formation/proxi/presentation/EditServlet.java
+++ src/fr/formation/proxi/presentation/EditServlet.java
@@ -19,7 +19,7 @@ public class EditServlet extends HttpServlet{
 
 @Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	
+	req.
 }
 
 }
