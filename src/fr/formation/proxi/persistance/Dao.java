d3939a9bff33a2e55f01ded3379bb03920ca77e9
SandyColin <sandycolin1990@gmail.com>
2018-11-26 14:15:01 +0100

    v2

--- src/fr/formation/proxi/persistance/Dao.java
+++ src/fr/formation/proxi/persistance/Dao.java
@@ -1,5 +1,35 @@
 package fr.formation.proxi.persistance;
 
-public interface Dao {
+import java.util.List;
+
+public interface Dao<T> {
+
+
+/**
+* 
+* Lit les informations d'une entité en BDD.
+* 
+* @param id identifiant unique de l'entité.
+* @return l'entité construite à partir des informations récupérées.
+*/
+public T read(Integer id);  //dans Hibernate, on utilise que des objets, aucun primitif.
+
+
+/**
+* Récupère toutes les entités d'une table (requête de type SELECT * FROM table). 
+* 
+* @return Une liste des entités récupérées. 
+*/
+public List<T> readAll();
+
+
+/**
+* Met à jour une entité d'une BDD.
+* @param entity L'entité à mettreà jour avec de nouvelles informations.
+* @return L'entité nouvellement mise à jour. 
+*/
+public T update(T entity); 
+
+
 
 }
